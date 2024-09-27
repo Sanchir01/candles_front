0c3bacc8f6d4df200a129c1f56f7339e7ad4fa68
anstonebear <anstonebear@mail.ru>
2024-09-27 21:44:09 +0300

    adaptive header and footer

--- src/widgets/header/ui/HeaderActions.tsx
+++ src/widgets/header/ui/HeaderActions.tsx
@@ -7,10 +7,16 @@ export const HeaderActions = () => {
 	return (
 		<div className={style.header_actions}>
 			<Link href='/favorites' className={style.header_actions_favorites}>
-				<Heart style={{ strokeWidth: 1 }} />
+				<Heart
+					style={{ strokeWidth: 1 }}
+					size={`clamp(0.625rem, 0.216rem + 2.05vw, 1.75rem)`}
+				/>
 			</Link>
 			<Link href='/cart' className={style.header_actions_cart}>
-				<ShoppingBasket style={{ strokeWidth: 1 }} />
+				<ShoppingBasket
+					style={{ strokeWidth: 1 }}
+					size={`clamp(0.625rem, 0.216rem + 2.05vw, 1.75rem)`}
+				/>
 			</Link>
 		</div>
 	)
