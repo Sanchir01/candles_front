a4ddcb8b4c1c739258e1a0e7610320a962375773
Sanchir01 <emgushovs@mail.ru>
2024-09-29 01:03:49 +0300

    add container ui component and create base react-query all candles

---
+++ src/entities/entitycandles/cart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const EntityProductCart = ({
+	children
+}: {
+	children: React.ReactNode
+}) => {
+	return (
+		<div>
+			<div className=''>{children}</div>
+		</div>
+	)
+}
---
+++ src/shared/hooks/allCandles.ts
@@ -0,0 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
+
+export const AllCandle = () => {
+	const { data } = useQuery({ queryFn })
+
+	return
+}
