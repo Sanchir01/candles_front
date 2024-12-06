b3e993e2b257ad76eed42c93f2e550f9373cc421
sanchir <emgushovs@mail.ru>
2024-12-06 22:45:37 +0300

    update package json

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,13 +1,11 @@
 import { QueryClient } from '@tanstack/react-query'
-//todo:switch time 1 mitnute 1*60*1000
 export const queryClient = new QueryClient({
    defaultOptions: {
       queries: {
          refetchOnWindowFocus: false,
          retry: 5,
          retryDelay: 2000,
-
-         staleTime: 0
+         staleTime: process.env.NODE_ENV === 'production' ? 3600 : 0
       }
    }
 })
