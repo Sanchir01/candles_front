0118977bb9a9c15624770dcb6ebdb4f758655490
sanchir <emgushovs@mail.ru>
2025-01-04 01:52:02 +0300

    mini update

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,5 @@
-import { cookies } from 'next/headers'
 import HomeContentComponent from '~/widgets/home'
 
 export default async function Home() {
-   const cookieStore = await cookies()
-   console.log('this Cookie', cookieStore)
    return <HomeContentComponent />
 }
