2d068514600c60f39ee517e1915e6b02e5e325a2
Sanchir <115372855+Sanchir01@users.noreply.github.com>
2025-01-12 02:37:08 +0300

    add yandex metrika

--- src/Providers/Provider.tsx
+++ src/Providers/Provider.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from 'react'
 import { ReactQueryProvider } from '~/Providers/react-query/reactquery'
 import { ThemeProvider } from '~/Providers/theme/theme-provider'
 import { ComposeChildren } from '~/shared/lib/react-utils'
+import YandexMetrika from './YMetrika'
 
 export const Provider = ({ children }: { children: ReactNode }) => {
    return (
       <ComposeChildren>
          <ThemeProvider />
          <ReactQueryProvider />
+         <YandexMetrika enabled={!!(process.env.NODE_ENV === 'production')} />
          {children}
       </ComposeChildren>
    )
